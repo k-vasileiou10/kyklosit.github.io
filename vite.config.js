83e2a004e1e6f26a8de110988a0e0fd514f0ad62
Konstantinos Vasileiou <150164529+k-vasileiou10@users.noreply.github.com>
2025-07-23 13:32:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kyklosit.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
